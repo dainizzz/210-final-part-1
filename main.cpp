59386245947715a2b389e36cd7d60de2969bd6e3
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-12-10 18:46:31 -0800

    10 minute update

--- main.cpp
+++ main.cpp
@@ -7,19 +7,32 @@ using namespace std;
 int main() {
 	// MILESTONE 1
 	map <string, int> airports;
-	string origin, destination, temp;
+	string origin, destination;
 	ifstream infile;
 
 	infile.open("210-final-1-FA25.txt");
 	if (infile.good()) {
 		while (infile >> origin >> destination) {
-			temp = origin + " " + destination;
-			auto it = airports.find(temp);
-			if (it != airports.end()) {}
+			auto search = airports.find(origin);
+			if (search == airports.end())
+				airports.insert(make_pair(origin, 1));
+			else
+				search->second++;
+
+			search = airports.find(destination);
+			if (search == airports.end())
+				airports.insert(make_pair(destination, 1));
+			else
+				search->second++;
 		}
 	}else {
 		cout << "Error opening file" << endl;
 		exit(2);
+	}
+	infile.close();
+
+	for (auto airport : airports) {
+		cout << airport.first << ": " << airport.second << endl;
 	}
 
 	return 0;
