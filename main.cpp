48f4f42933e41c34d38f0b08d743c5b341a9a960
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-12-10 18:37:07 -0800

    10 minute update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <map>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	// MILESTONE 1
+	map <string, int> airports;
+	string origin, destination, temp;
+	ifstream infile;
+
+	infile.open("210-final-1-FA25.txt");
+	if (infile.good()) {
+		while (infile >> origin >> destination) {
+			temp = origin + " " + destination;
+			auto it = airports.find(temp);
+			if (it != airports.end()) {}
+		}
+	}else {
+		cout << "Error opening file" << endl;
+		exit(2);
+	}
+
 	return 0;
 }
